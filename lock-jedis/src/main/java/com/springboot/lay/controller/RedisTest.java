c8c5149c7ad7bad51fc78c73cf39c7521ae62e10
Liangzifeng <liangzifenf@gmail.com>
2019-04-03 09:22:44 +0800

    springboot各种整合实例，持续更新

--- lock-jedis/src/main/java/com/springboot/lay/controller/RedisTest.java
+++ lock-jedis/src/main/java/com/springboot/lay/controller/RedisTest.java
@@ -1,10 +1,7 @@
 package com.springboot.lay.controller;
 
 import com.springboot.lay.common.RedisCommon;
-import com.springboot.lay.config.RedisConfig;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
--- redis/src/main/java/org/spring/springboot/controller/CityRestController.java
+++ redis/src/main/java/org/spring/springboot/controller/CityRestController.java
@@ -3,10 +3,8 @@ package org.spring.springboot.controller;
 import org.spring.springboot.domain.City;
 import org.spring.springboot.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 /**
  * Created by bysocket on 07/02/2017.
